bb945a89884b208da2c96e4f3b992405756e4d0d
SickdaFlip <philippbreitsprecher@gmail.com>
2016-09-01 13:19:16 +0200

    Update Data.php

--- app/code/community/Uni/Fileuploader/Helper/Data.php
+++ app/code/community/Uni/Fileuploader/Helper/Data.php
@@ -1,12 +1,4 @@
 <?php
-
-/**
- * Unicode Systems
- * @category   Uni
- * @package    Uni_Fileuploader
- * @copyright  Copyright (c) 2010-2011 Unicode Systems. (http://www.unicodesystems.in)
- * @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
- */
 class Uni_Fileuploader_Helper_Data extends Mage_Core_Helper_Abstract
 {
 
